bd308f228275c619a4bfbdb2f46d8cd998e528cb
Marcelo Silva <maarcelomonteiro@gmail.com>
2022-02-26 14:52:29 -0300

    feat: home finish

---
+++ src/scene/Home/home.styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components/native';
+
+export const TextEmpty = styled.Text`
+  color: #fafafa;
+  align-self: center;
+  margin: 20px 0px;
+`;
--- src/scene/Home/index.tsx
+++ src/scene/Home/index.tsx
@@ -1,11 +1,58 @@
-import React, {FC} from 'react';
-import {View} from 'react-native';
-// import { Wrapper } from './styles';
+import React, {FC, useState} from 'react';
+import {SafeAreaView, ScrollView} from 'react-native';
+import {gql} from 'apollo-boost';
+import {useQuery} from 'react-apollo';
+import {Country, Button} from '../../components';
+import {TextEmpty} from './home.styles';
 
-type Props = {};
+const GET_CONTINENTS = gql`
+  query GetContinents {
+    continents {
+      name
+      countries {
+        name
+        currency
+        emoji
+        native
+        phone
+        capital
+      }
+    }
+  }
+`;
 
-const Home: FC<Props> = () => {
-  return <View />;
+const Home: FC = () => {
+  const [country, setCountry] = useState<Country.DTOCountry[]>([]);
+
+  const {data} = useQuery(GET_CONTINENTS);
+
+  return (
+    <SafeAreaView style={{backgroundColor: 'rgb(32, 32, 36)', height: '100%'}}>
+      <TextEmpty>Escolha um continente</TextEmpty>
+      <ScrollView>
+        {data.continents.map(({name, countries}: Continents.DTOContinents) => (
+          <Button key={name} onPress={() => setCountry(countries)}>
+            {name}
+          </Button>
+        ))}
+        {country.map(
+          ({
+            name,
+            currency,
+            emoji,
+            native,
+            phone,
+            capital,
+          }: Country.DTOCountry) => (
+            <Country
+              key={name}
+              {...{name, currency, emoji, native, phone, capital}}
+            />
+          ),
+        )}
+      </ScrollView>
+    </SafeAreaView>
+  );
 };
 
 export default Home;
