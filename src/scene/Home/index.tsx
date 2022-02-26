c890903a731abceaa65c58a50fb6a8fdcadfea6a
Marcelo Silva <maarcelomonteiro@gmail.com>
2022-02-26 15:00:49 -0300

    fix: show when finish request

--- src/scene/Home/index.tsx
+++ src/scene/Home/index.tsx
@@ -24,17 +24,18 @@ const GET_CONTINENTS = gql`
 const Home: FC = () => {
   const [country, setCountry] = useState<Country.DTOCountry[]>([]);
 
-  const {data} = useQuery(GET_CONTINENTS);
+  const {data, loading} = useQuery(GET_CONTINENTS);
 
   return (
     <SafeAreaView style={{backgroundColor: 'rgb(32, 32, 36)', height: '100%'}}>
       <TextEmpty>Escolha um continente</TextEmpty>
       <ScrollView>
-        {data.continents.map(({name, countries}: Continents.DTOContinents) => (
-          <Button key={name} onPress={() => setCountry(countries)}>
-            {name}
-          </Button>
-        ))}
+        {!loading &&
+          data.continents.map(({name, countries}: Continents.DTOContinents) => (
+            <Button key={name} onPress={() => setCountry(countries)}>
+              {name}
+            </Button>
+          ))}
         {country.map(
           ({
             name,
