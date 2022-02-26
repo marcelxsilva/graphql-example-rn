3cb69d48399fe72cfafd8b446afc6bd4c6f9026d
Marcelo Silva <maarcelomonteiro@gmail.com>
2022-02-26 13:26:01 -0300

    feat: start project test

---
+++ src/scene/Home/index.tsx
@@ -0,0 +1,11 @@
+import React, {FC} from 'react';
+import {View} from 'react-native';
+// import { Wrapper } from './styles';
+
+type Props = {};
+
+const Home: FC<Props> = () => {
+  return <View />;
+};
+
+export default Home;
